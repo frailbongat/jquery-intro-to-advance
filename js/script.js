221e4aec028b25f6004e3003002aa69ae80271ab
Frail Bongat <frail.bongat@gmail.com>
2021-02-28 13:28:10 +0800

    Added fade methods

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-$(function() {
+$(function () {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  // $(".red-box").fadeOut(2000);
+  $('.red-box').fadeTo(3000, 0.2);
+  $('.green-box').fadeTo(2000, 0.5);
+  $('.blue-box').fadeTo(1000, 0.8);
+
+  $('.red-box').fadeToggle();
+  $('.red-box').fadeToggle();
 });
