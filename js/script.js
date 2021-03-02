18691c0bbe61ccdbed9478ad385dd8f87c731c22
Frail Bongat <frail.bongat@gmail.com>
2021-03-02 12:47:52 +0800

    Added showing and hiding elemnts

--- js/script.js
+++ js/script.js
@@ -2,10 +2,15 @@ $(function () {
   // jQuery goes here...
 
   // Uncomment this line to fade out the red box on page load
-  $('.red-box').fadeTo(3000, 0.2);
-  $('.green-box').fadeTo(2000, 0.5);
-  $('.blue-box').fadeTo(1000, 0.8);
+  // $('.red-box').fadeTo(3000, 0.2);
+  // $('.green-box').fadeTo(2000, 0.5);
+  // $('.blue-box').fadeTo(1000, 0.8);
 
-  $('.red-box').fadeToggle();
-  $('.red-box').fadeToggle();
+  // $('.red-box').fadeToggle();
+  // $('.red-box').fadeToggle();
+
+  // Showing and hiding elements
+  // $('.blue-box').hide();
+  // $('.blue-box').show();x
+  $('.blue-box').toggle(1000);
 });
